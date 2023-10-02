a5693a8729930f44c3b07ec1de4bc961587afd65
DesigningCodingCreating <108616359+DesigningCodingCreating@users.noreply.github.com>
2023-10-02 19:02:49 +0000

    Initial commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { DarkMode, Navbar, NavBrand, NavLi, NavUl, NavHamburger, TextPlaceholder, Card, Button } from 'flowbite-svelte';
+	export let page;
+	let isHomePage = page === "home";
+</script>
+
+
+<div class="relative px-8">
+  <Navbar navClass="px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 right-0 border-b" let:hidden let:toggle>
+    <div class="flex items-center justify-between w-full">
+      <div class="flex items-center space-x-4">
+        <NavBrand href="/">
+          <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Owen's Projects</span>
+        </NavBrand>
+        <NavHamburger on:click={toggle} />
+      </div>
+      <div class="flex items-center">
+		<NavUl {hidden}>
+			<NavLi href="/" active={true}>Home</NavLi>
+        </NavUl>
+        <DarkMode/>
+      </div>
+    </div>
+  </Navbar>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+	import Navbar from "../components/Navbar.svelte";
+</script>
+
+<Navbar></Navbar>
+
+<br><br><br>
+
+<div class="container mx-auto mt-8">
+    <div class="text-gray-600 dark:bg-slate-800 bg-white shadow-md rounded-lg p-4 mb-4">
+      <h5 class="text-gray-600 text-xl font-semibold mb-2 dark:text-white">Welcome to Owen's Projects!</h5>
+      <p class="text-gray-600 dark:text-slate-400">Hello, I'm from the UK, and I'm a developer with a passion for creating cool projects. I specialize in software development.</p>
+		<br>
+		<p class="text-gray-600 dark:text-slate-400">Some of my other hobbies include: </p>
+			<ul class="dark:text-slate-400 text-gray-600" style="list-style-type:circle">
+				<li>Gaming</li>
+				<li>Reading</li>
+				<li>CAD (Computer Aided Design)</li>
+			</ul>
+		<br>
+		<p class="text-gray-600 dark:text-slate-400">Online, I am known as DesigningCodingCreating or DesignCodeCreate.</p>
+    </div>
+</div>
+
+<div class="container mx-auto mt-8">
+    <div class="text-gray-600 dark:bg-slate-800 bg-white shadow-md rounded-lg p-4 mb-4">
+      <h5 class="text-gray-600 text-xl font-semibold mb-2 dark:text-white">OwenTheBot</h5>
+      <p class="text-gray-600 dark:text-slate-400">I've made a Discord bot that does some cool stuff.</p>
+		<p class="text-gray-600 dark:text-slate-400">You can see the bot's commands and functions with
+  <a class="listelement" href="https://owenthebot.owensprojects.com"><b>this link</b></a>.</p>
+		<br>
+		<p class="listelement text-gray-600 dark:text-slate-400">You can invite the bot to your Discord server <a class="listelement" href="https://discord.com/api/oauth2/authorize?client_id=973939317900734555&permissions=139586948160&scope=applications.commands%20bot"><b>here</b></a>.</p>
+    </div>
+</div>
+
+<div class="container mx-auto mt-8">
+    <div class="text-gray-600 dark:bg-slate-800 bg-white shadow-md rounded-lg p-4 mb-4">
+      <h5 class="text-gray-600 text-xl font-semibold mb-2 dark:text-white">FishyGames</h5>
+      <p class="text-gray-600 dark:text-slate-400"></p>
+		<p class="text-gray-600 dark:text-slate-400">This is a basic games website made with Svelte and TailwindCSS.</p>
+		<br>
+		<p class="text-gray-600 dark:text-slate-400">You can visit the website <a class="listelement" href="https://fishygames.owensprojects.com"><b>here</b></a>.</p>
+    </div>
+</div>
+
+
+<div class="container mx-auto mt-8">
+    <div class="text-gray-600 dark:bg-slate-800 bg-white shadow-md rounded-lg p-4 mb-4">
+		<h5 class="text-gray-600 text-xl font-semibold mb-2 dark:text-white">Some Links</h5>
+		<ul class="dark:text-slate-400 text-gray-600" style="list-style-type:circle">
+			<li class="listelement"><a href="https://github.com/DesigningCodingCreating"><b>GitHub</b> - DesigningCodingCreating</a></li>
+			<li class="listelement"><a href="https://discord.com/users/934133252397076540"><b>Discord</b> - DesigningCodingCreating</a></li>
+			<li class="listelement"><a href="https://namemc.com/profile/DesignCodeCreate"><b>Minecraft</b> - DesignCodeCreate</a></li>
+			<li class="listelement"><a href="https://replit.com/@DesigningCodingCreating"><b>Replit</b> - DesigningCodingCreating</a></li>
+		</ul>
+    </div>
+</div>
+
+
+<style>
+	ul {
+	  display: block;
+	  list-style-type: disc;
+	  margin-top: 1em;
+	  margin-bottom: 1 em;
+	  margin-left: 0;
+	  margin-right: 0;
+	  padding-left: 40px;
+	}
+	.listelement:hover {
+	  text-decoration: underline;
+	}
+</style>1
