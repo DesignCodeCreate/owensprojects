2341590e45c1af02ec6d98e25928f1f3b3858ce5
DesigningCodingCreating <108616359+DesigningCodingCreating@users.noreply.github.com>
2023-10-03 10:45:39 +0000

    Links Outside site and underline issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,9 +27,9 @@
       <h5 class="text-gray-600 text-xl font-semibold mb-2 dark:text-white">OwenTheBot</h5>
       <p class="text-gray-600 dark:text-slate-400">I've made a Discord bot that does some cool stuff.</p>
 		<p class="text-gray-600 dark:text-slate-400">You can see the bot's commands and functions with
-  <a class="listelement" href="https://owenthebot.owensprojects.com"><b>this link</b></a>.</p>
+  <a class="listelement" href="https://owenthebot.owensprojects.com" target="_blank"><b>this link</b></a>.</p>
 		<br>
-		<p class="text-gray-600 dark:text-slate-400">You can invite the bot to your Discord server <a class="listelement" href="https://discord.com/api/oauth2/authorize?client_id=973939317900734555&permissions=139586948160&scope=applications.commands%20bot"><b>here</b></a>.</p>
+		<p class="text-gray-600 dark:text-slate-400">You can invite the bot to your Discord server <a class="listelement" href="https://discord.com/api/oauth2/authorize?client_id=973939317900734555&permissions=139586948160&scope=applications.commands%20bot" target="_blank"><b>here</b></a>.</p>
     </div>
 </div>
 
@@ -39,7 +39,7 @@
       <p class="text-gray-600 dark:text-slate-400"></p>
 		<p class="text-gray-600 dark:text-slate-400">This is a basic games website made with Svelte and TailwindCSS.</p>
 		<br>
-		<p class="text-gray-600 dark:text-slate-400">You can visit the website <a class="listelement" href="https://fishygames.owensprojects.com"><b>here</b></a>.</p>
+		<p class="text-gray-600 dark:text-slate-400">You can visit the website <a class="listelement" href="https://fishygames.owensprojects.com" target="_blank"><b>here</b></a>.</p>
     </div>
 </div>
 
@@ -48,10 +48,10 @@
     <div class="text-gray-600 dark:bg-slate-800 bg-white shadow-md rounded-lg p-4 mb-4">
 		<h5 class="text-gray-600 text-xl font-semibold mb-2 dark:text-white">Some Links</h5>
 		<ul class="dark:text-slate-400 text-gray-600" style="list-style-type:circle">
-			<li class="listelement"><a href="https://github.com/DesigningCodingCreating"><b>GitHub</b> - DesigningCodingCreating</a></li>
-			<li class="listelement"><a href="https://discord.com/users/934133252397076540"><b>Discord</b> - DesigningCodingCreating</a></li>
-			<li class="listelement"><a href="https://namemc.com/profile/DesignCodeCreate"><b>Minecraft</b> - DesignCodeCreate</a></li>
-			<li class="listelement"><a href="https://replit.com/@DesigningCodingCreating"><b>Replit</b> - DesigningCodingCreating</a></li>
+			<li class="listelement"><a href="https://github.com/DesigningCodingCreating" target="_blank"><b>GitHub</b> - DesigningCodingCreating</a></li>
+			<li class="listelement"><a href="https://discord.com/users/934133252397076540" target="_blank"><b>Discord</b> - DesigningCodingCreating</a></li>
+			<li class="listelement"><a href="https://namemc.com/profile/DesignCodeCreate" target="_blank"><b>Minecraft</b> - DesignCodeCreate</a></li>
+			<li class="listelement"><a href="https://replit.com/@DesigningCodingCreating" target="_blank"><b>Replit</b> - DesigningCodingCreating</a></li>
 		</ul>
     </div>
 </div>
@@ -70,4 +70,4 @@
 	.listelement:hover {
 	  text-decoration: underline;
 	}
-</style>1
+</style>
