c761091aa3f0f2f5cb21e64682fd79aec8c8c799
Owen Hoskin <hoskinowen7@gmail.com>
2024-06-22 12:31:29 +0100

    Minor tweaks, removal of owenthebot page

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,7 +1,5 @@
 <script>
   import { DarkMode, Navbar, NavBrand, NavLi, NavUl, NavHamburger, TextPlaceholder, Card, Button } from 'flowbite-svelte';
-	export let page;
-	let isHomePage = page === "home";
 </script>
 
 
@@ -9,15 +7,9 @@
   <Navbar navClass="px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 right-0 border-b" let:hidden let:toggle>
     <div class="flex items-center justify-between w-full">
       <div class="flex items-center space-x-4">
-        <NavBrand href="/">
-          <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Owen's Projects</span>
-        </NavBrand>
-        <NavHamburger on:click={toggle} />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Owen's Projects</span>
       </div>
       <div class="flex items-center">
-		<NavUl {hidden}>
-			<NavLi href="/" active={true}>Home</NavLi>
-        </NavUl>
         <DarkMode/>
       </div>
     </div>
--- src/routes/owenthebot/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Navbar from "/src/components/Navbar.svelte";
-</script>
-
-<Navbar channel={"owenthebot"} />
-
-<br><br><br><br>
-
-<button src="https://discord.com/api/oauth2/authorize?client_id=973939317900734555&permissions=139586948160&scope=applications.commands%20bot"> click </button>
